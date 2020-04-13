681b277e73b228ce2ed84cc2c6a7d02b8bae47a1
andiniputrimr <a2.1800016@mhs.stmik-sumedang.ac.id>
2020-04-13 19:32:42 +0700

    praktikum php7

--- A2.1800016/siswa/config/database.php
+++ A2.1800016/siswa/config/database.php
@@ -4,9 +4,9 @@ date_default_timezone_set("ASIA/JAKARTA");
 
 // deklarasi parameter koneksi database
 $server   = "localhost";            // server database, default "localhost" atau "127.0.0.1"
-$username = "id13234747_andiniputrimr";                 // username database, default "root"
-$password = "Anptmr_/15091999";                     // password database, default kosong
-$database = "id13234747_sekolah";           // memilih database yang akan digunakan
+$username = "root";                 // username database, default "root"
+$password = "";                     // password database, default kosong
+$database = "db_sekolah";           // memilih database yang akan digunakan
 
 // koneksi database
 $db = mysqli_connect($server, $username, $password, $database);
