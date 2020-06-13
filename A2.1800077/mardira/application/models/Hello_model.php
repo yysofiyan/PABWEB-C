46f37cc22a32da44b73a0c34a7b739c0934b303d
Luthfi Izzuddin <A2.1800077@mhs.stmik-sumedang.ac.id>
2020-06-13 16:16:22 +0700

    praktikum codeigniter

---
+++ A2.1800077/mardira/application/models/Hello_model.php
@@ -0,0 +1,14 @@
+<?php
+
+class Hello_model extends CI_Model
+{
+	public function hellomardira()
+	{
+		return "Hello Aji";
+	}
+
+	public function aji()
+	{
+		return "Ini menggunakan MVC.";
+	}
+}
